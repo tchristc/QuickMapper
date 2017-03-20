c41ce2bb5c60cdfd8bebfaa6bf4672cb57514355
WiDpiTom <thomas.christensen@dpi.wi.gov>
2017-03-20 15:38:03 -0500

    battle with roslyn...fixed loading System.IO

---
+++ QuickMapper.Compiler/QuickMapperCompiler.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+
+namespace QuickMapper.Compiler
+{
+    public class QuickMapperCompiler
+    {
+        private static readonly IEnumerable<string> DefaultNamespaces =
+            new[]
+            {
+                        "System",
+                        "System.IO",
+                        "System.Net",
+                        "System.Linq",
+                        "System.Text",
+                        "System.Text.RegularExpressions",
+                        "System.Collections.Generic"
+            };
+
+
+        private static string runtimePath = @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.6.2\{0}.dll";
+        
+        private static readonly IEnumerable<MetadataReference> DefaultReferences =
+            new[]
+            {
+                        MetadataReference.CreateFromFile(string.Format(runtimePath, "mscorlib")),
+                        MetadataReference.CreateFromFile(string.Format(runtimePath, "System")),
+                        MetadataReference.CreateFromFile(string.Format(runtimePath, "System.Core"))
+            };
+
+        private static readonly CSharpCompilationOptions DefaultCompilationOptions =
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                    .WithOverflowChecks(true).WithOptimizationLevel(OptimizationLevel.Release)
+                    .WithUsings(DefaultNamespaces);
+
+        public static SyntaxTree Parse(string text, string filename = "", CSharpParseOptions options = null)
+        {
+            var stringText = SourceText.From(text, Encoding.UTF8);
+            return SyntaxFactory.ParseSyntaxTree(stringText, options, filename);
+        }
+
+        public void Test()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+                using System;
+
+                namespace RoslynCompileSample
+                {
+                    public class Writer
+                    {
+                        public void Write(string message)
+                        {
+                            Console.WriteLine(message);
+                        }
+                    }
+                }");
+
+            //var dd = typeof(Enumerable).GetTypeInfo().Assembly.Location;
+            //var coreDir = Directory.GetParent(dd);
+            
+            var assemblyName = Path.GetRandomFileName();
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                new[] { syntaxTree },
+                DefaultReferences,
+                DefaultCompilationOptions);
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    var failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (var diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var assembly = Assembly.Load(ms.ToArray());
+
+                    var type = assembly.GetType("RoslynCompileSample.Writer");
+                    var obj = Activator.CreateInstance(type);
+                    type.InvokeMember("Write",
+                        BindingFlags.Default | BindingFlags.InvokeMethod,
+                        null,
+                        obj,
+                        new object[] { "Hello World" });
+                }
+            }
+
+        }
+    }
+}
